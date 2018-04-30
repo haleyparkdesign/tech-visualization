321db1191e6c318709fc78dd7b67bc7ba8dd126e
hpark <hpark11@wellesley.edu>
2018-04-30 04:19:11 -0400

    Plot 3 progress. Todo: make it sortable and enable the toggle switch

--- sketch3.js
+++ sketch3.js
@@ -6,121 +6,106 @@ var gridLabelHeight = 18; // space reserved for gridline labels
 var gridChartOffset = 10; // space between start of grid and first bar
 var maxBarWidth = 700; // width of the bar with the max value
 
-d3.csv("diversity.csv", function (data) {
-    data.forEach(function (d) {
-        d.company = d.Company;
-        d.female = +d.Female;
-        d.male = +d.Male;
+var svg = d3.select("#plot3").append("svg")
+    .attr("width", maxBarWidth + companyLabelWidth + valueLabelWidth)
+    .attr("height", 800),
+    margin = {
+        top: 20,
+        right: 60,
+        bottom: 30,
+        left: 40
+    },
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scaleLinear()
+    .rangeRound([0, maxBarWidth]);
+
+var y = d3.scaleBand()
+    .rangeRound([0, width])
+    .padding(0.3)
+    .align(0.1);
+
+var colors = d3.scaleOrdinal()
+    .range(["#007F75", "#FF7B5C"]);
+
+var stack = d3.stack()
+    .offset(d3.stackOffsetExpand);
+
+d3.csv("diversity.csv", type, function (error, data) {
+    if (error) throw error;
+
+    data.sort(function (a, b) {
+        return b[data.columns[1]] / b.total - a[data.columns[1]] / a.total;
     });
 
-    console.log(data);
-    // accessor functions
-    var companyLabel = function (d) {
-        return d['Company'];
-    };
-    var femaleValue = function (d) {
-        return parseFloat(d['Female']);
-    };
+    y.domain(data.map(function (d) {
+        return d.Company;
+    }));
 
-    var maleValue = function (d) {
-        return parseFloat(d['male']);
-    };
+    colors.domain(data.columns.slice(1));
 
-    var total = function (d) {
-        return 100;
-    }
+    g.attr('transform', 'translate(' + companyLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')');
 
-    // sorting
-    var sortedData = data.sort(function (a, b) {
-        return d3.descending(femaleValue(a), femaleValue(b));
-    });
 
-    // scales
-    var yScale = d3.scaleBand().domain(d3.range(0, data.length)).rangeRound([0, data.length * barHeight * 2]);
+    var axisY = g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y))
 
-    var y = function (d, i) {
-        return yScale(i);
-    };
-    var yText = function (d, i) {
-        return y(d, i) + yScale.bandwidth() / 2;
-    };
-    var x = d3.scaleLinear()
-        .domain([0, d3.max(data, total)])
-        .range([0, maxBarWidth]);
+    axisY.selectAll("text")
+        .style("fill", "#282828")
+        .attr("font-size", "15px")
+        .attr("font-family", "Work Sans");
 
-    // svg container element
-    var plot3 = d3.select('#plot3').append("svg")
-        .attr('width', maxBarWidth + companyLabelWidth + valueLabelWidth)
-        .attr('height', gridLabelHeight + gridChartOffset + data.length * barHeight * 2);
+    axisY.selectAll("line")
+        .style("stroke", "transparent");
 
-    // grid line labels
-    var gridContainer = plot3.append('g')
-        .attr('transform', 'translate(' + companyLabelWidth + ',' + gridLabelHeight + ')');
+    var axisX = g.append("g")
+        .attr("class", "axis axis--x")
+        .call(d3.axisTop(x).ticks(10, "%"));
 
-    gridContainer.selectAll("text").data(x.ticks(10)).enter().append("text")
-        .attr("x", x)
-        .attr("dy", -3)
-        .attr("text-anchor", "middle")
-        .text(String);
-    // vertical grid lines
-    gridContainer.selectAll("line").data(x.ticks(10)).enter().append("line")
-        .attr("x1", x)
-        .attr("x2", x)
-        .attr("y1", 0)
-        .attr("y2", data.length * barHeight * 2 + gridChartOffset)
+    axisX.selectAll("line")
+        .attr("y1", function () {
+            return 3
+        })
+        .attr("y2", function () {
+            return 773
+        })
         .style("stroke", "#ccc");
 
-    // bar labels
-    var labelsContainer = plot3.append('g')
-        .attr('transform', 'translate(' + (companyLabelWidth - companyLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')');
+    axisX.selectAll("text")
+        .style("fill", "#282828")
+        .attr("font-size", "15px")
+        .attr("font-family", "Work Sans");
 
-    labelsContainer.selectAll('text').data(data).enter().append('text')
-        .attr('y', yText)
-        .attr('stroke', 'none')
-        .attr("dy", ".35em") // vertical-align: middle
-        .attr('text-anchor', 'end')
-        .text(companyLabel);
-
-    // bars
-    var barsContainer = plot3.append('g')
-        .attr('transform', 'translate(' + companyLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')');
-
-    barsContainer.selectAll("rect").data(data).enter().append("rect")
-        .attr('y', y)
-        .attr('height', 28)
-        .attr('width', function (d) {
-            return x(femaleValue(d));
+    var serie = g.selectAll(".serie")
+        .data(stack.keys(data.columns.slice(1))(data))
+        .enter().append("g")
+        .attr("class", "serie")
+        .attr("fill", function (d) {
+            return colors(d.key);
         })
-        .attr('stroke', 'white')
-        .attr('fill', '#007F75');
 
-    //    barsContainer.selectAll("rect").data(data).enter().append("rect")
-    //        .attr('y', y)
-    //        .attr('height', yScale.rangeBand())
-    //        .attr('width', function (d) {
-    //            return x(maleValue(d));
-    //        })
-    //        .attr('stroke', 'white')
-    //        .attr('fill', '#FF7B5C');
-
-    // bar value labels
-    barsContainer.selectAll("text").data(data).enter().append("text")
+    serie.selectAll("rect")
+        .data(function (d) {
+            return d;
+        })
+        .enter().append("rect")
         .attr("x", function (d) {
-            return x(femaleValue(d));
+            return x(d[0]);
         })
-        .attr("y", yText)
-        .attr("dx", 3) // padding-left
-        .attr("dy", ".35em") // vertical-align: middle
-        .attr("text-anchor", "start") // text-align: right
-        .attr("fill", "black")
-        .attr("stroke", "none")
-        .text(function (d) {
-            return Math.round(femaleValue(d), 2);
-        });
-
-    // start line
-    barsContainer.append("line")
-        .attr("y1", -gridChartOffset)
-        .attr("y2", data.length * barHeight * 2 + gridChartOffset)
-        .style("stroke", "#282828");
+        .attr("y", function (d) {
+            return y(d.data.Company);
+        })
+        .attr("width", function (d) {
+            return x(d[1]) - x(d[0]);
+        })
+        .attr("height", y.bandwidth());
 });
+
+function type(d, i, columns) {
+    for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+    d.total = t;
+    return d;
+}
