00f8b8f4260e52bd7f5f46b7af536f97faa0b662
hpark <hpark11@wellesley.edu>
2018-05-04 16:05:31 -0400

    Added sketch 1. Need to work on the styling

---
+++ sketch1.js
@@ -0,0 +1,107 @@
+var sketch1 = (function () { //use IIFE to avoid variable collision
+
+    // set the dimensions and margins of the graph
+    var margin = {
+            top: 20,
+            right: 20,
+            bottom: 30,
+            left: 50
+        },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    // set the ranges
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // define the 1st line
+    var menLine = d3.line()
+        .x(function (d) {
+            return x(d.Year);
+        })
+        .y(function (d) {
+            return y(d.men);
+        });
+
+    // define the 2nd line
+    var womenLine = d3.line()
+        .x(function (d) {
+            return x(d.Year);
+        })
+        .y(function (d) {
+            return y(d.women);
+        });
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#plot1").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // add the X gridlines
+    svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + width + ")")
+        .call(d3.axisBottom(x)
+            .ticks(10)
+            .tickSize(-width)
+            .tickFormat("")
+        ).selectAll("line").style("stroke", "#ccc");
+
+    // Get the data
+    d3.csv("wage_average_over_time.csv", type, function (error, data) {
+        console.log(data);
+        if (error) throw error;
+
+        // Scale the range of the data
+        x.domain(d3.extent(data, function (d) {
+            return d.Year;
+        }));
+        y.domain([0, d3.max(data, function (d) {
+            return Math.max(d.women, d.men);
+        })]);
+
+        // Add the valueline path.
+        svg.append("path")
+            .data([data])
+            .attr("class", "line")
+            .style("fill", "none")
+            .style("stroke", "ff7b5c").style("stroke-width", 2)
+            .attr("d", menLine);
+
+        // Add the valueline2 path.
+        svg.append("path")
+            .data([data])
+            .attr("class", "line")
+            .style("fill", "none")
+            .style("stroke", "007f75").style("stroke-width", 2)
+            .attr("d", womenLine);
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+        // Add the Y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+    });
+
+    var parseTime = d3.timeParse("%Y%m%d");
+
+    svg.selectAll("text")
+        .style("fill", "#282828")
+        .attr("font-size", "15px")
+        .attr("font-family", "Work Sans");
+
+    function type(d, i, columns) {
+        for (var i = 0, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
+        return d;
+    }
+})();
+
+sketch1;
--- sketch3.js
+++ sketch3.js
@@ -1,192 +1,197 @@
-var valueLabelWidth = 80; // space reserved for value labels (right)
-var companyLabelWidth = 120; // space reserved for bar labels
-var companyLabelPadding = 15; // padding between bar and bar labels (left)
-var gridLabelHeight = 18; // space reserved for gridline labels
-var gridChartOffset = 10; // space between start of grid and first bar
-var maxBarWidth = 700; // width of the bar with the max value
-var exec = false;
+var sketch3 = (function () { //use IIFE to avoid variable collision
+    var valueLabelWidth = 80; // space reserved for value labels (right)
+    var companyLabelWidth = 120; // space reserved for bar labels
+    var companyLabelPadding = 15; // padding between bar and bar labels (left)
+    var gridLabelHeight = 18; // space reserved for gridline labels
+    var gridChartOffset = 10; // space between start of grid and first bar
+    var maxBarWidth = 700; // width of the bar with the max value
+    var exec = false;
 
-var svg = d3.select("#plot3").append("svg")
-    .attr("width", maxBarWidth + companyLabelWidth + valueLabelWidth)
-    .attr("height", 698),
-    margin = {
-        top: 20,
-        right: 60,
-        bottom: 30,
-        left: 40
-    },
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .attr('transform', 'translate(' + companyLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')');
+    var svg = d3.select("#plot3").append("svg")
+        .attr("width", maxBarWidth + companyLabelWidth + valueLabelWidth)
+        .attr("height", 698),
+        margin = {
+            top: 20,
+            right: 60,
+            bottom: 30,
+            left: 40
+        },
+        width = +svg.attr("width"),
+        height = +svg.attr("height"),
+        g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr('transform', 'translate(' + companyLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')');
 
 
-var x = d3.scaleLinear()
-    .rangeRound([0, maxBarWidth]);
+    var x = d3.scaleLinear()
+        .rangeRound([0, maxBarWidth]);
 
-var y = d3.scaleBand()
-    .rangeRound([0, height])
-    .padding(0.3)
-    .align(0.1);
+    var y = d3.scaleBand()
+        .rangeRound([0, height])
+        .padding(0.3)
+        .align(0.1);
 
-var colors = d3.scaleOrdinal()
-    .range(["#007F75", "#FF7B5C"]);
+    var colors = d3.scaleOrdinal()
+        .range(["#007F75", "#FF7B5C"]);
 
-var stack = d3.stack()
-    .offset(d3.stackOffsetExpand);
+    var stack = d3.stack()
+        .offset(d3.stackOffsetExpand);
 
-var tooltip = d3.select("#plot3").append("div")
-    .attr("class", "toolTip")
-    .style("display", "none");
+    var tooltip = d3.select("#plot3").append("div")
+        .attr("class", "toolTip")
+        .style("display", "none");
 
-var thisData;
+    var thisData;
 
-// add the X gridlines
-g.append("g")
-    .attr("class", "grid")
-    .attr("transform", "translate(0," + width + ")")
-    .call(d3.axisBottom(x)
-        .ticks(10)
-        .tickSize(-width)
-        .tickFormat("")
-    ).selectAll("line").style("stroke", "#ccc");
+    // add the X gridlines
+    g.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + width + ")")
+        .call(d3.axisBottom(x)
+            .ticks(10)
+            .tickSize(-width)
+            .tickFormat("")
+        ).selectAll("line").style("stroke", "#ccc");
 
-var axisX = g.append("g")
-    .attr("class", "axis axis--x")
-    .call(d3.axisTop(x).ticks(10, "%"));
+    var axisX = g.append("g")
+        .attr("class", "axis axis--x")
+        .call(d3.axisTop(x).ticks(10, "%"));
 
-axisX.selectAll("text")
-    .style("fill", "#282828")
-    .attr("font-size", "15px")
-    .attr("font-family", "Work Sans");
+    axisX.selectAll("text")
+        .style("fill", "#282828")
+        .attr("font-size", "15px")
+        .attr("font-family", "Work Sans");
 
-var axisY = g.append("g")
-    .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y));
+    var axisY = g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y));
 
-axisY.selectAll("text")
-    .style("fill", "#282828")
-    .attr("font-size", "15px")
-    .attr("font-family", "Work Sans");
+    axisY.selectAll("text")
+        .style("fill", "#282828")
+        .attr("font-size", "15px")
+        .attr("font-family", "Work Sans");
 
-axisY.selectAll("line")
-    .style("stroke", "transparent");
+    axisY.selectAll("line")
+        .style("stroke", "transparent");
 
-d3.csv("diversity.csv", type, function (error, data) {
-    console.log(data);
-    draw();
+    d3.csv("diversity.csv", type, function (error, data) {
+        console.log(data);
+        draw(); //initial draw
 
-    d3.selectAll("#execSwitch").on("change", function () {
-        if (exec == false) {
-            exec = true;
-            g.selectAll(".bars").remove();
-            draw(exec);
-        } else {
-            exec = false;
-            g.selectAll(".bars").remove();
-            draw(exec);
-        }
-        console.log(exec);
-    })
-
-    d3.selectAll("#sort").on("change", function () {
-        if (this.value == "name") {
-            data.sort(function (a, b) {
-                return d3.descending(b.Company, a.Company);
-            });
-        } else if (this.value == "female") {
-            if (exec) {
-                data.sort(function (a, b) {
-                    return d3.ascending(b.FemaleLeadership, a.FemaleLeadership);
-                });
+        d3.selectAll("#execSwitch").on("change", function () {
+            if (exec == false) {
+                exec = true;
+                g.selectAll(".bars").remove();
+                draw(exec);
             } else {
+                exec = false;
+                g.selectAll(".bars").remove();
+                draw(exec);
+            }
+            console.log(exec);
+        })
+
+        d3.selectAll("#sort").on("change", function () {
+            if (this.value == "name") {
                 data.sort(function (a, b) {
-                    return d3.ascending(b.Female, a.Female);
+                    return d3.descending(b.Company, a.Company);
                 });
+            } else if (this.value == "female") {
+                if (exec) {
+                    data.sort(function (a, b) {
+                        return d3.ascending(b.FemaleLeadership, a.FemaleLeadership);
+                    });
+                } else {
+                    data.sort(function (a, b) {
+                        return d3.ascending(b.Female, a.Female);
+                    });
+                }
+
+            }
+            g.selectAll(".bars").remove(); //remove all bars before redraw
+            draw(exec);
+        });
+
+        if (error) throw error;
+
+        function draw(executive) {
+            y.domain(data.map(function (d) {
+                return d.Company;
+            }));
+
+            axisY.transition()
+                .call(d3.axisLeft(y));
+
+            if (executive) {
+                var bars = g.selectAll(".bars")
+                    .data(stack.keys(data.columns.slice(3, 6))(data))
+                    .enter().append("g")
+                    .attr("class", "bars")
+                    .attr("fill", function (d) {
+                        return colors(d.key);
+                    });
+            } else {
+                var bars = g.selectAll(".bars")
+                    .data(stack.keys(data.columns.slice(1, 3))(data))
+                    .enter().append("g")
+                    .attr("class", "bars")
+                    .attr("fill", function (d) {
+                        return colors(d.key);
+                    });
             }
 
+
+            var rects = bars.selectAll("rect")
+                .data(function (d) {
+                    return d;
+                })
+                .enter().append("rect")
+                .attr("x", function (d) {
+                    return x(d[0]);
+                })
+                .attr("width", function (d) {
+                    return x(d[1]) - x(d[0]);
+                })
+                .attr("height", y.bandwidth())
+
+                .attr("y", function (d) {
+                    return y(d.data.Company);
+                })
+
+            rects.on("mousemove", function (d) {
+                    tooltip
+                        .style("left", d3.event.pageX + 10 + "px")
+                        .style("top", d3.event.pageY - 70 + "px")
+                        .style("display", "inline-block")
+
+                    if (exec) {
+                        tooltip.html(d.data.Company + " - Leadership<br>" +
+                            "Female: " + d.data.FemaleLeadership + "%<br>" +
+                            "Male: " + d.data.MaleLeadership + "%");
+                    } else {
+                        tooltip.html(d.data.Company + " - All Employees<br>" +
+                            "Female: " + d.data.Female + "%<br>" +
+                            "Male: " + d.data.Male + "%<br>");
+                    }
+
+
+                })
+                .on("mouseout", function (d) {
+                    tooltip.style("display", "none");
+                });
+
+            colors.domain(data.columns.slice(1));
         }
-        g.selectAll(".bars").remove(); //remove all bars before redraw
-        draw(exec);
     });
 
-    if (error) throw error;
-
-    function draw(executive) {
-        y.domain(data.map(function (d) {
-            return d.Company;
-        }));
-
-        axisY.transition()
-            .call(d3.axisLeft(y));
-
-        if (executive) {
-            var bars = g.selectAll(".bars")
-            .data(stack.keys(data.columns.slice(3, 6))(data))
-            .enter().append("g")
-            .attr("class", "bars")
-            .attr("fill", function (d) {
-                return colors(d.key);
-            });
-        } else {
-            var bars = g.selectAll(".bars")
-            .data(stack.keys(data.columns.slice(1, 3))(data))
-            .enter().append("g")
-            .attr("class", "bars")
-            .attr("fill", function (d) {
-                return colors(d.key);
-            });
+    function type(d, i, columns) {
+        for (i = 1, t = 0; i < columns.length; ++i) {
+            t += d[columns[i]] = +d[columns[i]];
+            d.total = 100;
         }
-
-
-        var rects = bars.selectAll("rect")
-            .data(function (d) {
-                return d;
-            })
-            .enter().append("rect")
-            .attr("x", function (d) {
-                return x(d[0]);
-            })
-            .attr("width", function (d) {
-                return x(d[1]) - x(d[0]);
-            })
-            .attr("height", y.bandwidth())
-
-            .attr("y", function (d) {
-                return y(d.data.Company);
-            })
-
-        rects.on("mousemove", function (d) {
-                tooltip
-                    .style("left", d3.event.pageX + 10 + "px")
-                    .style("top", d3.event.pageY - 70 + "px")
-                    .style("display", "inline-block")
-
-            if (exec) {
-                tooltip.html(d.data.Company + " - Leadership<br>" +
-                          "Female: " + d.data.FemaleLeadership + "%<br>" +
-                          "Male: " + d.data.MaleLeadership + "%");
-            } else {
-                tooltip.html(d.data.Company + "- All Employees<br>" +
-                          "Female: " + d.data.Female + "%<br>" +
-                          "Male: " + d.data.Male + "%<br>");
-            }
-
-
-            })
-            .on("mouseout", function (d) {
-                tooltip.style("display", "none");
-            });
-
-        colors.domain(data.columns.slice(1));
+        return d;
     }
-});
 
-function type(d, i, columns) {
-    for (i = 1, t = 0; i < columns.length; ++i) {
-        t += d[columns[i]] = +d[columns[i]];
-        d.total = 100;
-    }
-    return d;
-}
+})();
+
+sketch3;
